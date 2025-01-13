d401a0f2283636809303980f129723f824766cfa
username <sharma.bablu832002@gmail.com>
2025-01-14 00:10:46 +0530

    nice day
    Day-1 nice experience

---
+++ React.js
@@ -0,0 +1,53 @@
+// how react works
+// Q1 what is react
+// React is js library
+// react is to help to create UI
+// rect is  a open source library
+// it is maintain developed facebook
+
+// Q2 features of React
+// jsx
+// Components
+// simplicity
+// performance
+// Virtual Dom
+// one way data binding
+
+// Q3 how react works
+// // React works is components form
+// React creates a virtual DOM. When state changes in a component it firstly runs a 
+// "diffing" algorithm, which identifies what has changed in the virtual DOM. 
+// The second step is reconciliation, where it updates the DOM with the results of diff.
+// what is key features of  mongodb
+
+// MongoDB is an open-source document database that provides high performance, high availability, and automatic scaling.
+
+// Its Key Features are:
+
+// Document Oriented and NoSQL database.
+// Supports Aggregation
+// Uses BSON format
+// Sharding (Helps in Horizontal Scalability)
+// Supports Ad Hoc Queries
+// Schema Less
+// Capped Collection
+// Indexing (Any field in MongoDB can be indexed)
+// MongoDB Replica Set (Provides high availability)
+// Supports Multiple Storage Engines
+// ACID compliance including multi-document transactions support (starting from v. 4)
+
+// what is primary key
+// primary key is notihing it is key of database
+// A PRIMARY KEY constraint is a unique identifier for a row within a database table.
+// Every table should have a primary key constraint to uniquely identify each row and only one primary key constraint can be created for each table.
+// The primary key constraints are used to enforce entity integrity.
+
+// what is npm
+
+// npm stands for Node Package Manager. npm provides the following two main functionalities:
+
+// Online repositories for Node.js packages/modules which are searchable on search.nodejs.org
+// Command-line utility to install packages, do version management and dependency management of Node.js packages.
+// Another important use for npm is dependency management. When you have a node project with a package.json file, you can run npm install from the project root and npm will install all the dependencies listed in the package.json.
+// what is git and SVN
+// The main point in Git vs SVN debate boils down to this: Git is a distributed version control system (DVCS), whereas SVN is a centralized version control system.
---
+++ ind.js
@@ -0,0 +1,15 @@
+// what is type of
+// typeof means is nothing it is operator in which type of variable and value
+// type of operator return  null 
+// let Name = "bablu";
+// console.log(typeof Name);
+
+// console.log(typeof null );
+
+// let  fruits = ["apple","banana","orange"]
+// console.log(typeof fruits)
+
+// let person = []
+// console.log(person)
+
+console.log(typeof name)
